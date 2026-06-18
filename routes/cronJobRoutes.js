77da7e0bc46da687e84cfc90a3d19091f07df9cd
MoemenAdam <mmuummnna@gmail.com>
2026-06-18 19:24:18 +0300

    Update error messages in cron job notification route for improved clarity and user feedback.

--- routes/cronJobRoutes.js
+++ routes/cronJobRoutes.js
@@ -8,11 +8,11 @@ Router.get('/send-notification', async (req, res, next) => {
     !req.headers.authorization ||
     !req.headers.authorization.startsWith('Bearer')
   ) {
-    return next(new AppError('You have to login first', 401));
+    return next(new AppError('who the hell are you ?', 401));
   }
   const token = req.headers.authorization.split(' ')[1];
   if (token !== process.env.cronSecret)
-    return next(new AppError('You have to login first', 401));
+    return next(new AppError('who the hell are you ?', 401));
   const data = await runNotificationCronJob();
 
   res.status(200).json({ status: 'success', data });
